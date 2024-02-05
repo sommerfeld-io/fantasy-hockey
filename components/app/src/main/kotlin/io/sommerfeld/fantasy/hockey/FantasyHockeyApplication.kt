0746b9efbb15358e7308f0274f3ef6508d0bf857
sebastian <sebastian@sommerfeld.io>
2024-02-05 17:42:24 +0100

    basic kotlin app

---
+++ components/app/src/main/kotlin/io/sommerfeld/fantasy/hockey/FantasyHockeyApplication.kt
@@ -0,0 +1,11 @@
+package io.sommerfeld.fantasy.hockey
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FantasyHockeyApplication
+
+fun main(args: Array<String>) {
+	runApplication<FantasyHockeyApplication>(*args)
+}
---
+++ components/app/src/test/kotlin/io/sommerfeld/fantasy/hockey/FantasyHockeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.sommerfeld.fantasy.hockey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FantasyHockeyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
